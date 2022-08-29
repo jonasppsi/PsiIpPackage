e8897e1195be6ecfca558bffd42d04bffb1c01d1
Patrick Studer <patrick.studer@enclustra.com>
2022-08-29 09:19:23 +0200

    WIP: First version of 2020_1 packager scripts

--- PsiIpPackage.tcl
+++ PsiIpPackage.tcl
@@ -12,14 +12,21 @@
 # (i.e. IP can be packaged with older versions also in future).
 #
 
-namespace eval psi::ip_package {
-  variable fileLoc [file normalize [file dirname [info script]]]
+namespace eval ::psi::ip_package {
+    variable fileLoc [file normalize [file dirname [info script]]]
+    source $fileLoc/scripts/utils/PsiUtilPath.tcl
+    source $fileLoc/scripts/utils/PsiUtilString.tcl
 }
 
-namespace eval psi::ip_package::2017_2_1 {
-	source "$psi::ip_package::fileLoc/scripts/IpPackage_2017_2_1.tcl"
+namespace eval ::psi::ip_package::2017_2_1 {
+	source "${::psi::ip_package::fileLoc}/scripts/IpPackage_2017_2_1.tcl"
 }
 
 namespace eval psi::ip_package::latest {
-	source "$psi::ip_package::fileLoc/scripts/IpPackage_2017_2_1.tcl"
+	source "${::psi::ip_package::fileLoc}/scripts/IpPackage_2017_2_1.tcl"
 }
+
+namespace eval psi::ip_package::2020_1 {
+	source "${::psi::ip_package::fileLoc}/scripts/IpPackage_2020_1.tcl"
+}
+          
